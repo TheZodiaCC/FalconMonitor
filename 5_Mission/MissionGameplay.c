37d04b1bcec53b8fe822ad18e90f098d0083c021
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-07-17 13:01:30 +0200

    Added closing PlayerMonitor with Escape Key

--- 5_Mission/MissionGameplay.c
+++ 5_Mission/MissionGameplay.c
@@ -11,32 +11,43 @@ modded class MissionGameplay {
 	override void OnKeyPress(int key)
     {
         super.OnKeyPress(key);
-		if (key == KeyCode.KC_M) 
+		if (key == KeyCode.KC_M || key == KeyCode.KC_ESCAPE) 
 		{
-			if ( PlayerMonit ) 
+			if (PlayerMonit) 
 			{
                 if (PlayerMonit.isMenuOpened()) 
 				{
-                    PlayerMonit.setMenuOpened(false);
-                    GetGame().GetUIManager().HideScriptedMenu(PlayerMonit);
-                    UnlockControls();
+                    closePlayerMenu();
                 } 
 				
-				else if (GetGame().GetUIManager().GetMenu() == NULL) 
+				else if (!PlayerMonit.isMenuOpened() && key == KeyCode.KC_M) 
 				{
-                    GetGame().GetUIManager().ShowScriptedMenu(PlayerMonit, NULL);
-                    PlayerMonit.setMenuOpened(true);
-					
-                    LockControls();
-               	 }
+                    openPlayerMenu();
+               	}
             } 
-			else if (GetGame().GetUIManager().GetMenu() == NULL && PlayerMonit == null) 
+			else if (!PlayerMonit && key == KeyCode.KC_M) 
 			{
                 LockControls();
                 PlayerMonit = PlayerMonitor.Cast(GetUIManager().EnterScriptedMenu(2138598, null));
                 PlayerMonit.setMenuOpened(true);
             }
 		}
+	}
+	
+	private void openPlayerMenu()
+	{
+		GetGame().GetUIManager().ShowScriptedMenu(PlayerMonit, NULL);
+        PlayerMonit.setMenuOpened(true);
+
+        LockControls();
+	}
+	
+	private void closePlayerMenu()
+	{
+		PlayerMonit.setMenuOpened(false);
+        GetGame().GetUIManager().HideScriptedMenu(PlayerMonit);
+		
+        UnlockControls();
 	}
 	
 	//To avoid strange situations when closing menu
