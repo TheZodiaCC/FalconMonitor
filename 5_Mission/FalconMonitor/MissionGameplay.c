984fc87e2ac05d681a94e2d0fd6b6d356d06390d
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-07-26 00:06:48 +0200

    tweaked closing/opening menu

--- 5_Mission/FalconMonitor/MissionGameplay.c
+++ 5_Mission/FalconMonitor/MissionGameplay.c
@@ -27,7 +27,6 @@ modded class MissionGameplay {
             } 
 			else if (!PlayerMonit && key == KeyCode.KC_M) 
 			{
-                LockControls();
                 PlayerMonit = PlayerMonitor.Cast(GetUIManager().EnterScriptedMenu(2138598, null));
                 PlayerMonit.setMenuOpened(true);
             }
@@ -38,31 +37,11 @@ modded class MissionGameplay {
 	{
 		GetGame().GetUIManager().ShowScriptedMenu(PlayerMonit, NULL);
         PlayerMonit.setMenuOpened(true);
-
-        LockControls();
 	}
 	
 	private void closePlayerMenu()
 	{
 		PlayerMonit.setMenuOpened(false);
         GetGame().GetUIManager().HideScriptedMenu(PlayerMonit);
-		
-        UnlockControls();
 	}
-	
-	//To avoid strange situations when closing menu
-	 private void LockControls()
-    {
-        GetGame().GetMission().PlayerControlDisable(INPUT_EXCLUDE_ALL);
-        GetGame().GetUIManager().ShowUICursor( true );
-        GetGame().GetMission().GetHud().Show( false );
-    }
-
-    private void UnlockControls()
-    {
-        GetGame().GetMission().PlayerControlEnable(false);
-        GetGame().GetInput().ResetGameFocus();
-        GetGame().GetUIManager().ShowUICursor( false );
-        GetGame().GetMission().GetHud().Show( true );
-    }
 }
--- 5_Mission/GUI/PlayerMonitor.c
+++ 5_Mission/GUI/PlayerMonitor.c
@@ -83,16 +83,14 @@ class PlayerMonitor extends UIScriptedMenu
 		super.OnHide();
         PPEffects.SetBlurMenu(0);
 
-        g_Game.GetUIManager().ShowCursor(true);
-        g_Game.GetUIManager().ShowUICursor(false);
-        GetGame().GetInput().ResetGameFocus();
-        GetGame().GetMission().PlayerControlEnable(false);
-        GetGame().GetMission().GetHud().Show( true );
+        unlockControls();
 	}
 	
 	override void OnShow() {
         super.OnShow();
         PPEffects.SetBlurMenu(0.5);
+		
+		lockControls();
     }
 	
 	private void initWidgets() {
@@ -211,6 +209,21 @@ class PlayerMonitor extends UIScriptedMenu
 		SkillsWidget.Show(true);
 		currentWidget = SkillsWidget;
 	}
+	
+	private void lockControls()
+    {
+        GetGame().GetMission().PlayerControlDisable(INPUT_EXCLUDE_ALL);
+        GetGame().GetUIManager().ShowUICursor( true );
+        GetGame().GetMission().GetHud().Show( false );
+    }
+
+    private void unlockControls()
+    {
+        GetGame().GetMission().PlayerControlEnable(false);
+        GetGame().GetInput().ResetGameFocus();
+        GetGame().GetUIManager().ShowUICursor( false );
+        GetGame().GetMission().GetHud().Show( true );
+    }
 }
 	
 	
